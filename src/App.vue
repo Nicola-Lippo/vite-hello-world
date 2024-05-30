c13c57043a3543924fef1ad088bd3fd5925c59b7
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-05-30 15:34:27 +0200

    milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script>
+// 1 step = importare il componente nel componente principale
+import AppTitle from './components/AppTitle.vue';
+// 2 step = registrare il componente all' interno del componente padre
+export default {
+  //PASCAL CASE
+  name: 'MyApp',
+  //e un oggetto?
+  components: {
+    AppTitle
 
+  }
+}
+</script>
+//3 step = aggiungere il tag con il nome del componente nel tag template
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppTitle />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,11 @@
+<script>
+//per importare correttamente un componente va anche esportato
+//default perchè c'è solo una cosa nel file
+//export default è un oggetto che se si vuole puo essergli assegnata una key name:
+export default {
+    name: 'AppTitle'
+}
+</script>
+<template>
+    <h1>La mia prima app con Vit</h1>
+</template>
