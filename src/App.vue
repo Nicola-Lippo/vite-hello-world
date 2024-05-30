9d1351eee0b7e23697dfa6c38f024b27db64aa09
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-05-30 15:45:40 +0200

    [UPD] + bonus

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <script>
 // 1 step = importare il componente nel componente principale
 import AppTitle from './components/AppTitle.vue';
+import AppJumbo from './components/AppJumbo.vue';
 // 2 step = registrare il componente all' interno del componente padre
 export default {
   //PASCAL CASE
   name: 'MyApp',
   //e un oggetto?
   components: {
-    AppTitle
+    AppTitle,
+    AppJumbo
 
   }
 }
 </script>
 //3 step = aggiungere il tag con il nome del componente nel tag template
+// 3 modi per scrivere un tag
 <template>
   <AppTitle />
+  <!--<AppTitle></AppTitle>-->
+  <!--<app-title />-->
+  <AppJumbo />
 </template>
 
 
---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+    name: 'AppJumbo'
+}
+</script>
+
+<template>
+    <h2>Ciao sono il secondo componente che creo con Vite</h2>
+</template>
